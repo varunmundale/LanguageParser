f81eeee37d0f1a33285e809bde4306099c8d7195
Varun Mundale <varunmundale@gmail.com>
2025-10-25 13:40:32 +0530

    analyser initialize

---
+++ src/main/java/org/example/AnalysisResult.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class AnalysisResult {
+    HashMap<MetricType, Integer> map;
+
+}
---
+++ src/main/java/org/example/IAnalyser.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface IAnalyser {
+    AnalysisResult analyse(String filePath);
+
+}
